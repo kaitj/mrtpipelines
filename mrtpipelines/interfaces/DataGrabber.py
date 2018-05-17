9af54867f4d2e9af52ff58d23a1963bb029499c1
Jason Kai <tkai@uwo.ca>
2018-05-17 15:00:36 -0400

    grab subjid from file and iterates act wf over subj

--- mrtpipelines/interfaces/DataGrabber.py
+++ mrtpipelines/interfaces/DataGrabber.py
@@ -1,17 +1,32 @@
-def getData(bids_dir, subjid):
-    from bids.grabbids import BIDSLayout
+from nipype import IdentityInterface
+from nipype.pipeline import engine as pe
 
-    layout = BIDSLayout(bids_dir)
+def getSubj(subjFile, work_dir):
+    # Retrieve individual subject ids
+    subjids = []
+    with open(subjFile) as f:
+        for subj in f:
+            temp = subj.lstrip('sub-')
+            temp = temp.rstrip('\n')
+            subjids.append(temp)
 
+    Subjid = pe.Node(IdentityInterface(fields=['subjid']), name='SubjectID')
+    Subjid.base_dir = work_dir
+    Subjid.iterables = [('subjid', subjids)]
+
+    return Subjid
+
+
+def getData(bids_layout, subjid):
     # Diffusion
-    nifti = layout.get(subject=subjid, modality='dwi', space='T1w', type='preproc',
+    nifti = bids_layout.get(subject=subjid, modality='dwi', space='T1w', type='preproc',
                                 return_type='file', extensions=['nii', 'nii.gz'])
-    bval = layout.get(subject=subjid, modality='dwi', space='T1w', type='preproc',
+    bval = bids_layout.get(subject=subjid, modality='dwi', space='T1w', type='preproc',
                                 return_type='file', extensions=['bval'])
-    bvec = layout.get(subject=subjid, modality='dwi', space='T1w', type='preproc',
+    bvec = bids_layout.get(subject=subjid, modality='dwi', space='T1w', type='preproc',
                                 return_type='file', extensions=['bvec'])
 
     # Freesurfer parcellation (from fmriprep)
-    parc = layout.get(subject=subjid, type='aseg', return_type='file', extensions=['mgz'])
+    parc = bids_layout.get(subject=subjid, type='aseg', return_type='file', extensions=['mgz'])
 
     return nifti[0], (bvec[0], bval[0]), parc[0]
