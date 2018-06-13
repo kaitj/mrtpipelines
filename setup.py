05d5a85907d47179a9c396f5fdca7698cb33f506
Jason Kai <tkai@uwo.ca>
2018-06-13 19:29:03 -0400

    updated set up for version 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     # Project information
     name='mrtrix_pipelines',
-    version='0.0.1',
+    version='1.0.0',
     description='Pipelines related to generating MRTrix tractography',
     packages=['mrtpipelines/interfaces',
               'mrtpipelines/workflows',
@@ -13,5 +13,5 @@ setup(
     # Metadata
     author='Jason Kai',
     author_email='tkai@uwo.ca',
-    url='https://github.com/kaitj/mrtrixpipelines'
+    url='https://github.com/kaitj/mrtpipelines'
 )
