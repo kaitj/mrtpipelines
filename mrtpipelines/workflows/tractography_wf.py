4d69b440766bd1d6c9892d61f61b688cc1e1f6a8
kaitj <tkai@uwo.ca>
2018-08-14 14:19:00 -0400

    revamped ACT tractography; moved more files around

--- mrtpipelines/workflows/tractography_wf.py
+++ mrtpipelines/workflows/tractography_wf.py
@@ -3,10 +3,54 @@ from nipype.interfaces import mrtrix3 as mrt
 
 import numpy as np
 
-def genTract_wf(nfibers=25000, wdir=None, nthreads=1,
-                name='genTract_wf'):
+def genACTTract_wf(nfibers=25000, wdir=None, nthreads=1,
+                   name='genACTTract_wf'):
     """
-    Set up workflow to generate tracts
+    Set up workflow to generate anatomically constrained tracts
+    """
+
+    # Define number of threads to use
+    if nthreads >= 8:
+        nthreads = np.int(nthreads / 4)
+
+    # Generate tract
+    genTract = pe.Node(mrt.Tractography(), name='genTract')
+    genTract.base_dir = wdir
+    genTract.inputs.backtract
+    genTract.inputs.ntracks = np.int(nfibers * 2)
+    genTract.inputs.out_file = 'variant-ACTtckgen-streamlines-%d_tract.tck' % (np.int(nfibers*2))
+    genTract.inputs.nthreads = nthreads
+    genTract.interface.num_threads = nthreads
+
+    # Spherical-deconvolution informed filtering of tractography
+    siftTract = pe.Node(mrt.SIFT(), name='siftTract')
+    siftTract.base_dir = wdir
+    siftTract.inputs.term_number = nfibers
+    siftTract.inputs.out_file = 'variant-ACTtcksift_streamlines-%d_track.tck' % nfibers
+    siftTract.inputs.nthreads = nthreads
+    siftTract.interface.num_threads = nthreads
+
+    # Convert to VTK
+    tractConvert = pe.Node(mrt.TCKConvert(), name='convTract')
+    tractConvert.base_dir = wdir
+    tractConvert.inputs.out_file = 'variant-ACTtcksift_streamlines-%d_tract.vtk' % nfibers
+    tractConvert.inputs.nthreads = nthreads
+    tractConvert.interface.num_threads = nthreads
+
+    # Build workflow
+    workflow = pe.Workflow(name=name)
+
+    workflow.connect([
+        (genTract, siftTract, [('out_file', 'in_file')]),
+        (siftTract, tractConvert, [('out_file', 'in_file')])
+    ])
+
+    return workflow
+
+def genDhollTract_wf(nfibers=25000, wdir=None, nthreads=1,
+                     name='genDhollTract_wf'):
+    """
+    Set up workflow to generate tracts with Dhollander response
     """
 
     # Define number of threads to use
@@ -26,7 +70,7 @@ def genTract_wf(nfibers=25000, wdir=None, nthreads=1,
     siftTract = pe.Node(mrt.Tractography(), name='siftTract')
     siftTract.base_dir = wdir
     siftTract.inputs.term_number = nfibers
-    siftTract.inputs.out_File = 'variant-tcksift_streamlines-%d_tract.tck' % nfibers
+    siftTract.inputs.out_file = 'variant-tcksift_streamlines-%d_tract.tck' % nfibers
     siftTract.inputs.nthreads = nthreads
     siftTract.interface.num_threads = nthreads
 
