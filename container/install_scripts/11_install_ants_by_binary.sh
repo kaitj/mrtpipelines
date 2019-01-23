947980d0b9de5a4248ac2ae2a2963b380f97df9f
kaitj <tkai@uwo.ca>
2019-01-23 13:30:27 -0500

    add ants to container, bump version

---
+++ container/install_scripts/11_install_ants_by_binary.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+D_DIR=/opt/ants
+
+if [ -d $D_DIR ]; then
+	rm -rf $D_DIR
+fi
+mkdir -p $D_DIR
+
+echo "curl -L --retry 5 https://github.com/ANTsX/ANTs/releases/download/v2.1.0/Linux_Ubuntu14.04.tar.bz2  | tar jx -C $D_DIR --strip-components=1"
+curl -L --retry 5 https://github.com/ANTsX/ANTs/releases/download/v2.1.0/Linux_Ubuntu14.04.tar.bz2  | tar jx -C $D_DIR --strip-components=1
+
+
+if [ -e $HOME/.profile ]; then #ubuntu
+	PROFILE=$HOME/.profile
+elif [ -e $HOME/.bash_profile ]; then #centos
+	PROFILE=$HOME/.bash_profile
+else
+	echo "Add PATH manualy: PATH=$D_DIR"
+	exit 0
+fi
+
+#check if PATH already exist in $PROFILE
+if grep -xq "export PATH=$D_DIR:\$PATH" $PROFILE #return 0 if exist
+then
+	echo "PATH=$D_DIR" in the PATH already.
+else
+	#create init script
+    echo "" >> $PROFILE
+    echo "#ants" >> $PROFILE
+	echo "export PATH=$D_DIR:\$PATH" >> $PROFILE
+	echo "export ANTSPATH=$D_DIR" >> $PROFILE
+fi
+
+#test installation
+source $PROFILE
+
+#test installation
+ants.sh -h >/dev/null
+if [ $? -eq 0 ]; then
+	echo "ANTS SUCCESS"
+	echo "To update PATH of current terminal: source $PFORFILE"
+	echo "To update PATH of all terminal: re-login"
+else
+    echo 'ANTS FAIL.'
+fi
