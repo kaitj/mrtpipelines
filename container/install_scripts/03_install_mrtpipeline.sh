8c7cf65983b006b2324f291891bdfd157a333833
kaitj <tkai@uwo.ca>
2020-03-11 15:45:33 -0400

    update pip

--- container/install_scripts/03_install_mrtpipeline.sh
+++ container/install_scripts/03_install_mrtpipeline.sh
@@ -8,6 +8,7 @@ git clone https://github.com/kaitj/mrtpipelines $DIR/mrtpipelines
 cd $DIR/mrtpipelines
 
 # Install requirements
+pip3 install --upgrade -pip
 pip3 install -r requirements.txt
 
 # Install mrtpipelines
